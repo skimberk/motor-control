e32d2f03c09ab183ea3e0e78782add91da45e016
Sebastian Kimberk <skimberk1@gmail.com>
2020-10-10 15:12:17 -0400

    organizing

--- matlab/pmsm_code.m
+++ matlab/pmsm_code.m
@@ -2,14 +2,14 @@ clear;
 
 pwm_frequency = 10000;
 phase_frequency = 10;
-V = 15;
+V = 75;
 
 % These are for the observer (the actual motor constants are in Simulink)
-R = (3/2) * 0.54;
+R = (3/2) * 0.34;
 L = (3/2) * 3.752e-4;
 psi = 0.1194;
 gamma = 1000;
 
-simulation_time_step = 0.000005;
+simulation_time_step = 0.00001;
 
 phase_frequency_radians = phase_frequency * 2 * pi;
