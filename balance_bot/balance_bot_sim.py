d88615ba1e95bbbf02c746a4509a3e15292f7fc6
Sebastian Kimberk <skimberk1@gmail.com>
2020-10-12 12:46:43 -0400

    removed unused comments

--- balance_bot/balance_bot_sim.py
+++ balance_bot/balance_bot_sim.py
@@ -2,24 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation
 
-# def _lambdifygenerated(m_r, m_w, g, l, r, tau, thetar, thetardot, thetaw, thetawdot):
-# 	return (
-# 		(-thetawdot*r*cos(thetar)/l - g*sin(thetar)/l - tau/(l**2*m_r)
-# 		(thetardot*l*m_r*r*cos(thetar) + thetaw*m_r*r**2 + tau)/(m_w*r**2))
-# 	)
-
-# def _lambdifygenerated(m_r, m_w, g, l, r, tau, thetar, thetardot, thetaw, thetawdot):
-# 	return (
-# 		-((1/2)*thetardot**2*l**2*m_r**2*r*sin(2*thetar) + g*l*m_r**2*r*sin(thetar) + g*l*m_r*m_w*r*sin(thetar) + l*m_r*tau*cos(thetar) + m_r*r*tau + m_w*r*tau)/(l**2*m_r*r*(m_r*sin(thetar)**2 + m_w)),
-# 		(thetardot**2*l**2*m_r*r*sin(thetar) + (1/2)*g*l*m_r*r*sin(2*thetar) + l*tau + r*tau*cos(thetar))/(l*r**2*(m_r*sin(thetar)**2 + m_w))
-# 	)
-
-# def _lambdifygenerated(m_r, m_w, g, l, r, t, tau, thetar, thetardot, thetaw, thetawdot):
-# 	return (
-# 		-1/2*thetardot**2*l**2*m_r**2*r*sin(2*thetar) + g*l*m_r**2*r*sin(thetar) + g*l*m_r*m_w*r*sin(thetar) - l*m_r*tau*cos(thetar) - m_r*r*tau - m_w*r*tau)/(l**2*m_r*r*(m_r*sin(thetar)**2 + m_w)),
-# 		(thetardot**2*l**2*m_r*r*sin(thetar) - 1/2*g*l*m_r*r*sin(2*thetar) + l*tau + r*tau*cos(thetar))/(l*r**2*(m_r*sin(thetar)**2 + m_w)
-# 	)
-
 l = 3
 r = 0.5
 g = 10
@@ -48,7 +30,7 @@ def rk4(f, x, h):
 
 	return x + h * (k1 + 2*k2 + 2*k3 + k4) / 6
 
-state = np.array([0, 15, 0, 0])
+state = np.array([0.5, 0, 0, 0])
 
 figure, axes = plt.subplots()
 
@@ -56,30 +38,23 @@ circle = plt.Circle((0, r), r, fill=False)
 rotation_indicator, = plt.plot([0, r], [r, r])
 rod, = plt.plot([0, 0], [r, r + l])
 
-last_error = np.pi - state[0] # + 0.01 * (state[1])
-
 K = np.array([[-146.7957, -3.1623, -55.7160, -6.2080]])
 
 def animate(i):
 	global state
 	global tau
-	# global last_error
-
-	# error = np.pi - state[0] # + 0.01 * (state[1])
-	# tau = 300 * error + 50 * (error - last_error) / 0.05
-	# last_error = error
 
 	tau = -(K @ state)[0]
-	# # tau = 0
 
-	state = rk4(model, state, 0.025)
+	for i in range(10):
+		state = rk4(model, state, 0.002)
 
 	circle.center = [r * state[1], r]
 	rotation_indicator.set_data([r * state[1], r * state[1] + r * np.cos(-state[1])], [r, r + r * np.sin(-state[1])])
 	rod.set_data([r * state[1], r * state[1] + l * np.cos(-state[0] + np.pi/2)], [r, r + l * np.sin(-state[0] + np.pi/2)])
 	return [circle, rotation_indicator, rod]
 
-anim = matplotlib.animation.FuncAnimation(figure, animate, interval=50, blit=True)
+anim = matplotlib.animation.FuncAnimation(figure, animate, interval=20)
 
 axes.add_artist(circle)
 plt.title('Rod on Wheel')
