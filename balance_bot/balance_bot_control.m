db4d24f66b305bac48796804f3cfde7b37330597
Sebastian Kimberk <skimberk1@gmail.com>
2020-10-12 12:08:30 -0400

    LQR control is working!!! did some matlab calculation to find gains, switched rod theta so upright is 0 (not pi), and simulated!!

---
+++ balance_bot/balance_bot_control.m
@@ -0,0 +1,20 @@
+clear;
+
+% These matrices obtained from Python notebook
+A = [0 0 1 0
+     0 0 0 1
+     110/3 0 0 0
+     -200 0 0 0];
+
+B = [0; 0; -71/90; 14/3];
+
+rank(ctrb(A, B)) % Since rank is 4, it's controllable!!
+
+Q = [1 0 0 0
+     0 0.1 0 0
+     0 0 0.01 0
+     0 0 0 0.01];
+ 
+R = [0.01];
+
+lqr(A, B, Q, R)
--- balance_bot/balance_bot_sim.py
+++ balance_bot/balance_bot_sim.py
@@ -8,11 +8,17 @@ import matplotlib.animation
 # 		(thetardot*l*m_r*r*cos(thetar) + thetaw*m_r*r**2 + tau)/(m_w*r**2))
 # 	)
 
-def _lambdifygenerated(m_r, m_w, g, l, r, tau, thetar, thetardot, thetaw, thetawdot):
-	return (
-		-((1/2)*thetardot**2*l**2*m_r**2*r*sin(2*thetar) + g*l*m_r**2*r*sin(thetar) + g*l*m_r*m_w*r*sin(thetar) + l*m_r*tau*cos(thetar) + m_r*r*tau + m_w*r*tau)/(l**2*m_r*r*(m_r*sin(thetar)**2 + m_w)),
-		(thetardot**2*l**2*m_r*r*sin(thetar) + (1/2)*g*l*m_r*r*sin(2*thetar) + l*tau + r*tau*cos(thetar))/(l*r**2*(m_r*sin(thetar)**2 + m_w))
-	)
+# def _lambdifygenerated(m_r, m_w, g, l, r, tau, thetar, thetardot, thetaw, thetawdot):
+# 	return (
+# 		-((1/2)*thetardot**2*l**2*m_r**2*r*sin(2*thetar) + g*l*m_r**2*r*sin(thetar) + g*l*m_r*m_w*r*sin(thetar) + l*m_r*tau*cos(thetar) + m_r*r*tau + m_w*r*tau)/(l**2*m_r*r*(m_r*sin(thetar)**2 + m_w)),
+# 		(thetardot**2*l**2*m_r*r*sin(thetar) + (1/2)*g*l*m_r*r*sin(2*thetar) + l*tau + r*tau*cos(thetar))/(l*r**2*(m_r*sin(thetar)**2 + m_w))
+# 	)
+
+# def _lambdifygenerated(m_r, m_w, g, l, r, t, tau, thetar, thetardot, thetaw, thetawdot):
+# 	return (
+# 		-1/2*thetardot**2*l**2*m_r**2*r*sin(2*thetar) + g*l*m_r**2*r*sin(thetar) + g*l*m_r*m_w*r*sin(thetar) - l*m_r*tau*cos(thetar) - m_r*r*tau - m_w*r*tau)/(l**2*m_r*r*(m_r*sin(thetar)**2 + m_w)),
+# 		(thetardot**2*l**2*m_r*r*sin(thetar) - 1/2*g*l*m_r*r*sin(2*thetar) + l*tau + r*tau*cos(thetar))/(l*r**2*(m_r*sin(thetar)**2 + m_w)
+# 	)
 
 l = 3
 r = 0.5
@@ -30,8 +36,8 @@ def model(x):
 	return np.array([
 		thetardot,
 		thetawdot,
-		-((1/2)*thetardot**2*l**2*m_r**2*r*np.sin(2*thetar) + g*l*m_r**2*r*np.sin(thetar) + g*l*m_r*m_w*r*np.sin(thetar) + l*m_r*tau*np.cos(thetar) + m_r*r*tau + m_w*r*tau)/(l**2*m_r*r*(m_r*np.sin(thetar)**2 + m_w)),
-		(thetardot**2*l**2*m_r*r*np.sin(thetar) + (1/2)*g*l*m_r*r*np.sin(2*thetar) + l*tau + r*tau*np.cos(thetar))/(l*r**2*(m_r*np.sin(thetar)**2 + m_w))
+		(-1/2*thetardot**2*l**2*m_r**2*r*np.sin(2*thetar) + g*l*m_r**2*r*np.sin(thetar) + g*l*m_r*m_w*r*np.sin(thetar) - l*m_r*tau*np.cos(thetar) - m_r*r*tau - m_w*r*tau)/(l**2*m_r*r*(m_r*np.sin(thetar)**2 + m_w)),
+		(thetardot**2*l**2*m_r*r*np.sin(thetar) - 1/2*g*l*m_r*r*np.sin(2*thetar) + l*tau + r*tau*np.cos(thetar))/(l*r**2*(m_r*np.sin(thetar)**2 + m_w))
 	])
 
 def rk4(f, x, h):
@@ -42,7 +48,7 @@ def rk4(f, x, h):
 
 	return x + h * (k1 + 2*k2 + 2*k3 + k4) / 6
 
-state = np.array([2.5, -15, 0, 0])
+state = np.array([0, 15, 0, 0])
 
 figure, axes = plt.subplots()
 
@@ -52,20 +58,25 @@ rod, = plt.plot([0, 0], [r, r + l])
 
 last_error = np.pi - state[0] # + 0.01 * (state[1])
 
+K = np.array([[-146.7957, -3.1623, -55.7160, -6.2080]])
+
 def animate(i):
 	global state
 	global tau
-	global last_error
+	# global last_error
 
-	error = np.pi - state[0] # + 0.01 * (state[1])
-	tau = 300 * error + 50 * (error - last_error) / 0.05
-	last_error = error
+	# error = np.pi - state[0] # + 0.01 * (state[1])
+	# tau = 300 * error + 50 * (error - last_error) / 0.05
+	# last_error = error
 
-	state = rk4(model, state, 0.05)
+	tau = -(K @ state)[0]
+	# # tau = 0
+
+	state = rk4(model, state, 0.025)
 
 	circle.center = [r * state[1], r]
 	rotation_indicator.set_data([r * state[1], r * state[1] + r * np.cos(-state[1])], [r, r + r * np.sin(-state[1])])
-	rod.set_data([r * state[1], r * state[1] + l * np.cos(state[0] - np.pi/2)], [r, r + l * np.sin(state[0] - np.pi/2)])
+	rod.set_data([r * state[1], r * state[1] + l * np.cos(-state[0] + np.pi/2)], [r, r + l * np.sin(-state[0] + np.pi/2)])
 	return [circle, rotation_indicator, rod]
 
 anim = matplotlib.animation.FuncAnimation(figure, animate, interval=50, blit=True)
